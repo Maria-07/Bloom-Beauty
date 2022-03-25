4d4dfbfef9d72316c58156a8c89a6605cda83f04
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-03-26 00:29:20 +0600

    product card design complete

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import "./Product.css";
 
 const Product = ({ product }) => {
   //   console.log(product);
@@ -11,6 +14,10 @@ const Product = ({ product }) => {
         <h6 className="product-name">{name}</h6>
         <p>Price : {price}$</p>
       </div>
+      <button className="card-btn">
+        <p>Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon>
+      </button>
     </div>
   );
 };
