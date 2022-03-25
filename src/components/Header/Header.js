af8ad707eb3c6b62abdc5d419785db5064659516
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-03-25 22:27:48 +0600

    Header part Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Bloom <span>Beauty</span>
+      </h1>
+      <h3>NEW ARRIVALS</h3>
+    </div>
+  );
+};
+
+export default Header;
